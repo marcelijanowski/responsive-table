0600e77c8d5de82b109238c6bf57456df90e7fcc
Marceli Janowski <marceli@mjdesign.pl>
2016-09-06 09:01:08 +0100

    Implement debounce function for window resize

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ $(function() {
       $(this).css('display', 'none');
     });
   };
+
   /**
   * Update paggination
   */
@@ -38,5 +39,43 @@ $(function() {
     updatePagination();
   });
 
+  $(window).smartresize(function(){
+    if ($(window).width() <= 740) {
+      updatePagination();
+    } else {
+      $('tbody tr').each(function () {
+        $(this).removeAttr('style');
+      });
+    }
+  });
   updatePagination();
 });
+
+// As user can scale windows size we have to react on changes cause by hidding element by mobile size
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
